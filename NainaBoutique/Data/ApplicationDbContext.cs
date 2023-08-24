6afde661a461d4bcc912633eceb1d1809f09236f
neelufarnidhin <nilu.nidhin@gmail.com>
2023-08-24 17:36:27 +0400

    Initial Commit

---
+++ NainaBoutique/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NainaBoutique.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
